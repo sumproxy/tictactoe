27147bc274e0cf86a4bfcfee61602338e7ac7948
Sum Proxy <sum.proxy@gmail.com>
2016-09-14 11:44:15 +0300

    Refactor Board into a separate file

--- src/main.rs
+++ src/main.rs
@@ -2,77 +2,9 @@ extern crate piston_window;
 extern crate find_folder;
 
 use piston_window::*;
-use std::ops::{Index, IndexMut};
     
-#[derive(Copy, Clone, PartialEq)]
-enum Player { X, O }
-
-struct Board([[Option<Player>; 3]; 3]);
-
-impl Board {
-    fn new() -> Self {
-        Board([[None; 3]; 3])
-    }
-
-    fn winner(&self, last_move: (usize, usize)) -> Option<Player> {
-        let (y, x) = last_move;
-        let board = self.0;
-        let player = board[y][x];
-
-        // check row
-        if board[y][0] == player && board[y][1] == player && board[y][2] == player {
-            return player;
-        }
-
-        // check column
-        if board[0][x] == player && board[1][x] == player && board[2][x] == player {
-            return player;
-        }
-
-        match (y, x) {
-            // not on a diagonal
-            (0, 1) | (1, 0) | (1, 2) | (2, 1) => None,
-            
-            (y, x) => {
-                // on a primary diagonal
-                if y == x && board[0][0] == player && board[1][1] == player && board[2][2] == player {
-                    player
-                }
-                // on a secondary diagonal
-                else if y + x == 2 && board[0][2] == player && board[1][1] == player && board[2][0] == player {
-                    player
-                }
-                // everything else
-                else {
-                    None
-                }
-            }
-        }
-    }
-}
-
-impl Index<usize> for Board {
-    type Output = [Option<Player>; 3];
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.0[index]
-    }
-}
-
-impl IndexMut<usize> for Board {
-    fn index_mut<'a>(&'a mut self, index: usize) -> &'a mut [Option<Player>; 3] {
-        &mut self.0[index]
-    }
-}
-
-impl<'a> IntoIterator for &'a Board {
-    type Item = &'a [Option<Player>; 3];
-    type IntoIter = std::slice::Iter<'a, [Option<Player>; 3]>;
-    
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
+mod board;
+use board::{Board, Player};
 
 fn main() {
     let black = [0.0, 0.0, 0.0, 1.0];
@@ -80,7 +12,7 @@ fn main() {
     
     let mut window: PistonWindow =
         WindowSettings::new("Tic Tac Toe!", [300, 300])
-        .resizable(true)
+        .resizable(false)
         .build()
         .unwrap();
 
